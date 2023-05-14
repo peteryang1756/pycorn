6837e0f0a2116395524ce4d91fe81f2e808fa307
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-14 13:13:12 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import time
+
+import requests
+
+while True:
+
+    # 在這裡插入你的corn job代碼
+
+    # 例如，如果你想要訪問某個API，可以使用下面的代碼：
+
+    response = requests.get('https://example.com/api')
+
+    print(response.json())
+
+    # 你可以在這裡插入你的corn job代碼結束
+
+    # 暫停5秒
+
+    time.sleep(5)
